a6d80aaa145235efeb1426a9b94e045b5abdaf1b
Caleb <reateguicevallosc@gmail.com>
2025-04-29 23:12:17 -0500

    Update: Database

--- quickrecap/settings.py
+++ quickrecap/settings.py
@@ -80,29 +80,22 @@ WSGI_APPLICATION = 'quickrecap.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_quickrecap',
+        'USER': 'db_quickrecap_user',
+        'PASSWORD': 'zoqD9qwbwIgsaOlEukfrAuIP1XrVx8iA',
+        'HOST': 'dpg-d08q0h95pdvs739p3d2g-a.oregon-postgres.render.com',
+        'PORT': '5432'
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'quickrecap-db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432'
-#     }
-# }
 
 
 # Password validation
